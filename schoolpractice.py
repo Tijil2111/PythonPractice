44d7081d7c5ad8033c11d6c935b26909c14bc4a0
Tijil2111 <tijil2008@gmail.com>
2021-06-30 23:27:39 +0530

    Made a simple mathematical calculator

--- schoolpractice.py
+++ schoolpractice.py
@@ -1,1 +1,12 @@
-print("Hello World")
+#Lets try to make a simple calculator where we edit the values in the code itself 
+
+num1 = 4
+num2 = 95
+#num3 if you want 
+
+print (num1*num2) # add the variable name in the bracket for which you wanna perform the mathematical operation
+                 # + for add 
+                   # - for sub
+                   # * for multiplication
+                   # / for divide 
+                   # % for remainder 
