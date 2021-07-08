55035b84f9f2a63635902886fed30f0eca5ec3ac
Tijil2111 <tijil2008@gmail.com>
2021-07-08 14:37:57 +0530

    Added basic Structure

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,13 @@
 # Making new calculator using the new functions taught by Sunita Maam in school 
 
 
+print("This is a Calculating machine which does every possible mathematical calculations")
+num1 = float(input("Please Tell Your 1st Number: "))
+num2 = float(input("Please Tell Your 2nd Number: "))
+
+
+print("Press 1 for addition ")
+print("Press 2 for subtraction ")
+print("Press 3 for multiplication ")
+print("Press 4 for division ")
+operator = int(input(print("Tell your preference")))
