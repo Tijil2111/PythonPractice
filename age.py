05a3fcf3ad88b76bf9532eb2460d380cf3e64290
Tijil <tijil2008@gmail.com>
2021-07-15 12:25:17 +0530

    Practised a new question

---
+++ age.py
@@ -0,0 +1,10 @@
+#Calculate if a person is Eligible for voting 
+
+
+age =int(input("Please Enter your age: "))
+
+if(age>=18):
+    print("Congrats! You can vote ")
+
+else:
+    print("Sorry you cant vote")
