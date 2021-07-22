ee6c83995e946e1587618648363cf2c34dda424c
Tijil <tijil2008@gmail.com>
2021-07-22 12:26:37 +0530

    Modified code

--- PracticeQuestion.py
+++ PracticeQuestion.py
@@ -3,22 +3,25 @@ unit = int(input("Please enter the units: "))
 
 total = price*unit
 
-print("Now we will calculate the GST and Discount")
+print("Now we will calculate the Net Amount Payable")
 
 if(total >= 15000):
     discount = total*15/100
     gst = total*18/100
-    print("The Net Amount is", total-discount+gst)
 
 elif(total >= 10000 & total < 15000):
     discount = total*10/100
     gst = total*15/100
-    print("The Net Amount is", total-discount+gst)
 
 elif(total <= 10000):
     discount = total*5/100
     gst = total*10/100
-    print("The Net Amount is", total-discount+gst)
 
 else:
     print("Not applicable")
+
+
+print("GST IS", gst)
+print("Discount is", discount)
+nap = total-discount+gst
+print("The Net Amount is", total-discount+gst)
