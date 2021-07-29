04f31f6112471cf570ad214da9f8ded63b649540
Tijil <tijil2008@gmail.com>
2021-07-29 12:22:04 +0530

    Practice Question 3

---
+++ PracticeQuestion3.py
@@ -0,0 +1,16 @@
+# To display fare
+
+
+destination = input("Enter your destination: ")
+
+if(destination == "delhi"):
+    print("The fare is Rs 4000")
+
+elif(destination == "mumbai"):
+    print("The fare is Rs 5000")
+
+elif(destination == "chennai"):
+    print("The fare is Rs 7000")
+
+else:
+    print("The fare for any other city is Rs 10000")
