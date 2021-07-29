2cf09e31e4c38eebe619610651849867eba367cf
Tijil <tijil2008@gmail.com>
2021-07-29 12:59:34 +0530

    Tried Python PPT

---
+++ TryingAllPythonCodefromPPT.py
@@ -0,0 +1,46 @@
+# Trying to do all practice questions
+
+
+# Trying print function
+
+print("Hello World")
+
+
+# Seprators
+
+print("Sum of 2 and 3 is", 2+3)
+
+# /t for tab
+
+print("Hello \t hello")
+
+
+# /n for newline
+
+print("Hello \n Hello ")
+
+
+# creating variables
+
+age = 12
+marks = 99.2
+city = "Delhi"
+
+
+# Comments
+
+# This is a single line comment
+
+""" This is a multi line comment """
+
+# Sum, minus, product, divide, divide in integers, exponet, remainder
+
+a = 1
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a % b)
